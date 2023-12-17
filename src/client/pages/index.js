80fec3b17387559c7c439af6117fb25dc91a4116
Koray Aydemir <korayaydemir276@gmail.com>
2023-12-17 17:30:36 +0300

    minimal working example for chatting (via console logs)

--- src/client/pages/index.js
+++ src/client/pages/index.js
@@ -1,57 +1,45 @@
 import * as domain from "../domain/index.js";
 import * as view from "../views/index.js";
 import * as p2pService from "../services/p2p/index.js";
-import * as mediaService from "../services/media/index.js";
 
 const p2p = p2pService.p2p;
 
-//p2p.createPeer((peerId) => {
-//    view.showReadyToCallContent(peerId);
-//});
-//
-//document.querySelector(".call-btn").addEventListener("click", () => {
-//    const code = (() => window.prompt("please enter the sharing code"))();
-//    domain.handleCall(p2p, view, { code });
-//});
-//
-//document.querySelector(".hangup-btn").addEventListener("click", () => {
-//    domain.handleHangup(p2p, view);
-//});
-//
-//p2p.onConnection();
-//
-//p2p.answerCall({
-//    onAccepted: () => {
-//        view.showConnectedContent();
-//    },
-//    onStreamReceived: (stream) => {
-//        view.setRemoteStream(stream);
-//    },
-//    onConnectionClosed: () => {
-//        console.log("connection closed");
-//        view.showReadyToCallContent();
-//    },
-//    onDenied: () => {
-//        console.log("call denied");
-//    },
-//});
-//
-//mediaService.getLocalStream();
-/////////////////////// second take
+const { onPeerReady, onIncomingConnection } =
+    domain.setUpPeer(p2p);
 
-p2p.createPeer((peerId) => {
-    view.showReadyToCallContent(peerId);
+onPeerReady((peerId) => {
+    view.showReadyToConnectContent(peerId);
 });
 
-p2p.onConnectionEvent();
+onIncomingConnection({
+    answerConnection: () => {
+        const shouldConnect = window.confirm(
+            "Do you want to accept the connection?",
+        );
 
-document.querySelector(".call-btn").addEventListener("click", () => {
+        return { shouldConnect };
+    },
+    onConnected: () => {
+        view.showConnectedContent();
+    },
+});
+
+document.querySelector(".join-btn").addEventListener("click", () => {
     const code = (() => window.prompt("please enter the sharing code"))();
-    p2p.connectPeers(code);
-    view.showConnectedContent();
+    const onConnectionAccepted = domain.joinChat(p2p, { code });
+
+    onConnectionAccepted(() => {
+        view.showConnectedContent();
+    });
 });
 
-document.querySelector(".hangup-btn").addEventListener("click", () => {
-    p2p.sendMessage();
+document.querySelector(".leave-btn").addEventListener("click", () => {
+    domain.leaveChat(p2p, view);
+    view.showReadyToConnectContent();
+});
 
-})
+document.querySelector(".send-msg-btn").addEventListener("click", () => {
+    let msgInput = document.querySelector("#msg-input");
+    domain.sendMessage(p2p, msgInput.value);
+    msgInput.value = "";
+});
